bf311d4b4afbceae980e54dfc9be3e4711fa2d43
Md Ashraful Islam <ms785314@gmail.com>
2024-11-07 13:03:34 +0600

    update page.tsx

--- app/playlists/page.tsx
+++ app/playlists/page.tsx
@@ -1,17 +1,21 @@
 "use client";
+import PlaylistsAccordion from "@/components/PlaylitsAccordion";
 import { usePlaylists } from "@/hooks/usePlaylists";
 import React from "react";
 
 const PlaylistsPage = () => {
   const { error, isLoading, playlists } = usePlaylists();
-  console.log("playlists", playlists);
   if (isLoading) {
     return <div>Loading Playlists..</div>;
   }
   if (error) {
     return <div>Error loading playlists: {error.message}</div>;
   }
-  return <section className="container mx-auto py-2">playlists..</section>;
+  return (
+    <section className="container mx-auto py-2">
+      <div>{playlists && <PlaylistsAccordion playlists={playlists} />}</div>
+    </section>
+  );
 };
 
 export default PlaylistsPage;
