67ff46f3aa89de221017bcac146b13727a63f814
Md Ashraful Islam <ms785314@gmail.com>
2024-11-07 13:33:15 +0600

    update page.tsx

--- app/topics/page.tsx
+++ app/topics/page.tsx
@@ -15,18 +15,31 @@ const TopicsPage = () => {
   return (
     <section className="py-4">
       <div className="container mx-auto p-4">
-        <h2 className="text-2xl font-bold mb-4">Topics</h2>
-        {topics?.map((topic) => (
-          <div
-            key={topic._id}
-            className="mb-4 p-4 border border-gray-200 rounded-md flex items-center justify-between"
-          >
-            <h3 className="text-xl font-semibold mb-2">{topic.title}</h3>
-            <Link href={`topics/${topic._id}`}>
-              <button className="btn btn-primary">Explore</button>
-            </Link>
-          </div>
-        ))}
+        <div className="flex text-center items-center flex-col max-w-[600px] mx-auto pb-3">
+          <h2 className="text-2xl font-bold mb-4 text-center">
+            Organize & Master Your Learning
+          </h2>
+          <p>
+            Collect and organize essential YouTube playlists in one place.
+            Whether you are diving into web development, design, or any new
+            skill, keep track of your progress and stay focused without
+            distractions.
+          </p>
+        </div>
+
+        <div className="grid grid-cols-4 gap-3">
+          {topics?.map((topic) => (
+            <div
+              key={topic._id}
+              className="mb-4 p-4 border border-gray-200 rounded-md flex items-center justify-between"
+            >
+              <h3 className="text-xl font-semibold mb-2">{topic.title}</h3>
+              <Link href={`topics/${topic._id}`}>
+                <button className="btn btn-primary">Explore</button>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
       <AddTopicForm />
     </section>
