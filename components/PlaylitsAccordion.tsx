c0acb173fb6652a465c0d979a83a173e5de0dd8b
Md Ashraful Islam <ms785314@gmail.com>
2024-11-07 18:13:46 +0600

    update PlaylitsAccordion.tsx and YouTubePlayer.tsx

--- components/PlaylitsAccordion.tsx
+++ components/PlaylitsAccordion.tsx
@@ -30,14 +30,17 @@ const PlaylistsAccordion = ({
   const [open, setOpen] = useState(false);
   return (
     <div className="flex gap-2 justify-center py-3">
-      {videoId && <div
-        onClick={() => setOpen(!open)}
-        className={clsx("cursor-pointer", {
-          "fixed right-1 top-30 border rounded-full shadow-lg p-2 z-20": videoId,
-        })}
-      >
-        <MdOpenInNew size={30} />
-      </div>}
+      {videoId && (
+        <div
+          onClick={() => setOpen(!open)}
+          className={clsx("cursor-pointer", {
+            "fixed right-1 top-30 border rounded-full shadow-lg p-2 z-20":
+              videoId,
+          })}
+        >
+          <MdOpenInNew size={30} />
+        </div>
+      )}
       {videoId && <YouTubePlayer videoId={videoId} />}
       <div
         className={clsx("flex gap-2 flex-col", {
@@ -47,81 +50,84 @@ const PlaylistsAccordion = ({
         })}
       >
         <Accordion type="single" collapsible>
-            {playlists.map((playlist) => (
-              <AccordionItem
-                key={playlist.playlistId}
-                value={playlist.playlistId}
-                className={clsx("border px-2 rounded-lg w-full p-3")}
-              >
-                <AccordionTrigger className="text-xl font-medium flex items-center space-x-4">
-                  <Image
-                    height={60}
-                    width={60}
-                    src={playlist.thumbnail}
-                    alt={playlist.title}
-                    className="w-16 h-16 object-cover rounded"
-                  />
-                  {/* Aligning the title to the left */}
-                  <span className="text-left w-full">{playlist.title}</span>
-                </AccordionTrigger>
-                <AccordionContent>
-                  <p className="text-gray-600 mb-4">{playlist.description}</p>
-                  <ul className="space-y-2">
-                    {playlist.videos.map((video) => (
-                      <li
-                        onClick={() => setVideoId(video.videoId)}
-                        key={video.videoId}
-                        className="flex items-center space-x-4 cursor-pointer"
-                      >
-                        <BsFillCameraVideoFill size={18} />
-                        <div
-                          //   onClick={() =>
-                          //     window.open(
-                          //       `https://www.youtube.com/watch?v=${video.videoId}`,
-                          //       "_blank"
-                          //     )
-                          //   }
-                          className="flex-1 hover:bg-gray-200 p-2 rounded"
-                        >
-                          <p className="text-md font-semibold">{video.title}</p>
-                        </div>
-                        {onClickMarkVideoAsFinished && (
-                          <div className="min-w-[30px]">
-                            {finishedVideos?.includes(video.videoId) ? (
-                              <button>
-                                <FaCheckCircle
-                                  size={25}
-                                  className="text-green-400"
-                                />
-                              </button>
-                            ) : (
-                              <div
-                                onClick={() =>
-                                  onClickMarkVideoAsFinished(
-                                    playlist.playlistId,
-                                    video.videoId
-                                  )
-                                }
-                                className="group"
-                              >
-                                <FaCheckCircle
-                                  className="group-hover:hidden"
-                                  size={25}
-                                />
-                                <button className="hidden group-hover:inline-block btn btn-primary text-white">
-                                  Mark Finished
-                                </button>
-                              </div>
-                            )}
-                          </div>
+          {playlists.map((playlist) => (
+            <AccordionItem
+              key={playlist.playlistId}
+              value={playlist.playlistId}
+              className={clsx("border px-2 rounded-lg w-full p-3")}
+            >
+              <AccordionTrigger className="text-xl font-medium flex items-center space-x-4">
+                <Image
+                  height={60}
+                  width={60}
+                  src={playlist.thumbnail}
+                  alt={playlist.title}
+                  className="w-16 h-16 object-cover rounded"
+                />
+                {/* Aligning the title to the left */}
+                <span className="text-left w-full">{playlist.title}</span>
+              </AccordionTrigger>
+              <AccordionContent>
+                <p className="text-gray-600 mb-4">{playlist.description}</p>
+                <ul className="space-y-2">
+                  {playlist.videos.map((video) => (
+                    <li
+                      onClick={() => setVideoId(video.videoId)}
+                      key={video.videoId}
+                      className="flex items-center space-x-4 cursor-pointer"
+                    >
+                      <BsFillCameraVideoFill size={18} />
+                      <div
+                        //   onClick={() =>
+                        //     window.open(
+                        //       `https://www.youtube.com/watch?v=${video.videoId}`,
+                        //       "_blank"
+                        //     )
+                        //   }
+                        className={clsx(
+                          "flex-1 hover:bg-gray-200 p-2 rounded",
+                          { "bg-gray-200": video.videoId === videoId }
                         )}
-                      </li>
-                    ))}
-                  </ul>
-                </AccordionContent>
-              </AccordionItem>
-            ))}
-          </Accordion>
+                      >
+                        <p className="text-md font-semibold">{video.title}</p>
+                      </div>
+                      {onClickMarkVideoAsFinished && (
+                        <div className="min-w-[30px]">
+                          {finishedVideos?.includes(video.videoId) ? (
+                            <button>
+                              <FaCheckCircle
+                                size={25}
+                                className="text-green-400"
+                              />
+                            </button>
+                          ) : (
+                            <div
+                              onClick={() =>
+                                onClickMarkVideoAsFinished(
+                                  playlist.playlistId,
+                                  video.videoId
+                                )
+                              }
+                              className="group"
+                            >
+                              <FaCheckCircle
+                                className="group-hover:hidden"
+                                size={25}
+                              />
+                              <button className="hidden group-hover:inline-block btn btn-primary text-white">
+                                Mark Finished
+                              </button>
+                            </div>
+                          )}
+                        </div>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
       </div>
     </div>
   );
